6d736ec2f2d160272f68388ce80c59a5982f13bc
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-03-24 18:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
  var newObj = Object.assign({}, object, {
     key: value
   });
+  
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
