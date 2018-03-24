e365778b12badee774e845ad1fd8b23e45650a1e
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-03-24 19:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
  var newObject = Object.assign({}, object)
-  delete Object.assign(object);
+  delete Object.assign(newObject);
   return newObject
 }
 
